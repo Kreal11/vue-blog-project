90266ce054e7f0e0c5c8505fb83d2568f1adaf2d
Kreal11 <kirillshyrokov2@gmail.com>
2023-12-16 01:40:48 +0200

    made architecture of vue project, added deploy gh-pages, added form and formList, added homepage

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <post-form @create="createPost" />
+    <post-list :posts="posts" />
+  </div>
 </template>
 
+<script>
+import PostForm from "@/components/PostForm.vue";
+import PostList from "./components/PostList.vue";
+export default {
+  components: {
+    PostForm,
+    PostList,
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "JS", body: "Post description" },
+        { id: 2, title: "JS 2", body: "Post description 2" },
+        { id: 3, title: "JS 3", body: "Post description 3" },
+        { id: 4, title: "JS 4", body: "Post description 4" },
+      ],
+    };
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+    },
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.app {
+  padding: 20px;
 }
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,23 @@
+<template lang="">
+  <div class="post" v-for="post in posts">
+    <div><strong>Name: </strong> {{ post.title }}</div>
+    <div><strong>Description</strong> {{ post.body }}</div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+<style scoped>
+.post {
+  padding: 15px;
+  border: 2px solid darkgreen;
+  margin-top: 15px;
+}
+</style>
