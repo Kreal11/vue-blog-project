9e7fe966282331010de8a1995cf2f878af1769d5
Kreal11 <kirillshyrokov2@gmail.com>
2023-12-17 00:18:22 +0200

    added animation for list items (adding, deleting, filter)

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length">
     <h3>Users list</h3>
-    <post-item
-      :post="post"
-      v-for="post in posts"
-      :key="post.id"
-      @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list">
+      <post-item
+        :post="post"
+        v-for="post in posts"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h2 v-else="!posts.length" style="color: green">There are no posts yet</h2>
 </template>
@@ -22,4 +24,21 @@ export default {
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.post-list-move {
+  transition: transform 0.6s ease;
+}
+</style>
